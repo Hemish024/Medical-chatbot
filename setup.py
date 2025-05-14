75eb71ee7e02ec1fc8dd9b85b1ede7a46ab2ccf7
hemish contractor <hemishcontractor241@gmail.com>
2025-05-14 14:45:16 -0400

    Folder structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+  name = 'generative AI Project',
+  version = '0.0.0',
+  author = 'Hemish',
+  author_email= 'hemishcontractor241@gmail.com',
+  packages = find_packages(),
+  install_requires = []
+)
